4e4a18e10fb4acbd09d97569b4c99daee6867c60
wachcio <wachcio@poczta.fm>
2021-05-25 02:14:45 +0200

    basic app

---
+++ App.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+import styled from 'styled-components';
+import WebView from 'react-native-webview';
+
+const App = () => {
+  console.log('Start log');
+
+  return (
+    <SafeAreaView>
+      <Container>
+        <DynamicWebView
+          source={{uri: 'http://meteo.wachcio.pl/'}}
+          startInLoadingState={true}
+          renderLoading={() => <Text>{'Loading...'}</Text>}
+          renderError={() => <Text>{'Error!'}</Text>}
+          onShouldStartLoadWithRequest={request => {
+            console.log('WEBVIEW request = ', request.mainDocumentURL);
+
+            if (request.mainDocumentURL.includes('wiadomosci')) {
+              return false;
+            }
+
+            return true;
+          }}
+          onMessage={msg => {
+            console.log('WEBVIEW msg = ', msg);
+          }}
+        />
+      </Container>
+    </SafeAreaView>
+  );
+};
+
+const Container = styled(View)`
+  width: 100%;
+  height: 100%;
+`;
+
+const DynamicWebView = styled(WebView)`
+  width: 100%;
+  height: 100%;
+`;
+
+export default App;
